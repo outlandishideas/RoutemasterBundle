fcadf5fcf22d2c7b37c7a0956de31c6ae48096d4
Tamlyn Rhodes <tamlyn@tamlyn.org>
2013-10-25 14:42:24 +0100

    Autoload service config

---
+++ DependencyInjection/OutlandishRoutemasterExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Outlandish\RoutemasterBundle\DependencyInjection;
+
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class OutlandishRoutemasterExtension extends Extension {
+
+	/**
+	 * Loads a specific configuration.
+	 *
+	 * @param array $config An array of configuration values
+	 * @param ContainerBuilder $container A ContainerBuilder instance
+	 *
+	 * @throws \InvalidArgumentException When provided tag is not defined in this extension
+	 *
+	 * @api
+	 */
+	public function load(array $config, ContainerBuilder $container) {
+		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+		$loader->load('services.yml');
+	}
+
+	public function getAlias() {
+		return 'outlandish_routemaster';
+	}
+}
