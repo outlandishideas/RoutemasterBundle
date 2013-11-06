8c9151b140a7100d6357b01f31f6abc69a4504c7
Tamlyn Rhodes <tamlyn@tamlyn.org>
2013-11-06 18:29:52 +0000

    Handle WP previews

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -20,7 +20,8 @@ class BaseController extends SymfonyController {
     {
         $scheme = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
         $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        if ("$scheme://$_SERVER[HTTP_HOST]$path" != get_permalink($post)) {
+	    $isPreview = isset($_GET['preview']) && $_GET['preview'] == 'true';
+	    if ("$scheme://$_SERVER[HTTP_HOST]$path" != get_permalink($post) && !$isPreview) {
             wp_redirect(get_permalink($post));
             die;
         }
@@ -40,18 +41,28 @@ class BaseController extends SymfonyController {
 
 	/**
 	 * Select a single post, set globals and throw 404 exception if nothing matches
-	 * @param $args
+	 * @param $queryArgs
 	 * @param bool $redirectCanonical true if should redirect canonically after fetching the post
 	 * @throws NotFoundHttpException
 	 * @return Post
 	 */
-    protected function querySingle($args, $redirectCanonical = false)
+    protected function querySingle($queryArgs, $redirectCanonical = false)
     {
         global $post;
-        $query = $this->query($args);
+
+	    //check for post preview and override query
+	    if (isset($_GET['preview']) && $_GET['preview'] == 'true' && (isset($_GET['p']) || $_GET['preview_id'])) {
+		    $queryArgs = array(
+			    'preview' => 'true',
+			    'p' => isset($_GET['p']) ? $_GET['p'] : $_GET['preview_id']
+		    );
+	    }
+
+        $query = $this->query($queryArgs);
+
         //no matched posts so 404
         if (!count($query->posts)) {
-            throw new NotFoundHttpException('No posts matching query '.json_encode($args));
+            throw new NotFoundHttpException('No posts matching query '.json_encode($queryArgs));
         }
 
         $post = $query->post;
